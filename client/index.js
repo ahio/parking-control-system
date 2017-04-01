4d6002f1645967a29fd337d85340aec0c1d0ee68
ahio <stepan.te.ua@gmail.com>
2017-04-01 17:11:51 +0300

    store results to localStorage

--- client/index.js
+++ client/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import ParkingControlView from './components/parking-control';
 
+window.parking = {
+    busyByHandicapped: () => localStorage['parking.getBusyHandicappedSlots'],
+    busyBySedan: () => localStorage['parking.getBusySedanSlots'],
+    busyByTruck: () => localStorage['parking.getBusyTruckSlots'],
+    getBusySlots: () => localStorage['parking.getBusySlots'],
+    freeHandicapped: () => localStorage['parking.getFreeHandicappedSlots'],
+    freeSedan: () => localStorage['parking.getFreeSedanSlots'],
+    freeTruck: () => localStorage['parking.getFreeTruckSlots'],
+    getFreeSlots: () => localStorage['parking.getFreeSlots']
+};
+
 ReactDOM.render(
     <ParkingControlView carSeats={30} />,
     document.getElementById('container')
--- client/modules/parking.js
+++ client/modules/parking.js
@@ -37,6 +37,7 @@ export default class ParkingControl {
             return s;
         });
 
+        this._updateAvailableSlots();
         return slot.id;
     }
 
@@ -58,5 +59,34 @@ export default class ParkingControl {
 
             return false;
         })[0];
+    }
+
+    _updateAvailableSlots() {
+        const slots = {
+            getFreeSlots: 0,
+            getFreeSedanSlots: 0,
+            getFreeTruckSlots: 0,
+            getFreeHandicappedSlots: 0,
+            getBusySlots: 0,
+            getBusySedanSlots: 0,
+            getBusyTruckSlots: 0,
+            getBusyHandicappedSlots: 0
+        };
+
+        this.parkingSlots.forEach((s) => {
+            slots[s.available ? 'getFreeSlots' : 'getBusySlots'] += 1;
+
+            if (s.type === 'sedan') {
+                slots[s.available ? 'getFreeSedanSlots' : 'getBusySedanSlots'] += 1;
+            } else if(s.type === 'truck') {
+                slots[s.available ? 'getFreeTruckSlots' : 'getBusyTruckSlots'] += 1;
+            } else {
+                slots[s.available ? 'getFreeHandicappedSlots' : 'getBusyHandicappedSlots'] += 1;
+            }
+        });
+
+        for(let field in slots) {
+            localStorage.setItem(`parking.${field}`, slots[field]);
+        }
     }
 };
